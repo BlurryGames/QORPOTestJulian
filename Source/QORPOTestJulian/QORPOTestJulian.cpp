0f4ce3a80f6bce5c31a2852e960a5ce762023dd0
BlurryGames <julian.lb.games@gmail.com>
2025-05-22 11:53:09 +0200

    Initial commit

---
+++ Source/QORPOTestJulian/QORPOTestJulian.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "QORPOTestJulian.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, QORPOTestJulian, "QORPOTestJulian" );
